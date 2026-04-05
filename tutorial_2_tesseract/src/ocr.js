a2a727fbc8a5970622932ee88123867bf219d158
frank-f-s <fshi0010@student.monash.edu>
2026-04-05 21:53:37 +1000

    Completed TesseractOCR tutorial

---
+++ tutorial_2_tesseract/src/ocr.js
@@ -0,0 +1,18 @@
+import { createWorker } from 'tesseract.js';
+
+
+(async () => {
+    // 1. Create a worker
+    const worker = await createWorker('eng'); // Specify the language (e.g., 'eng' for English)
+
+    // 2. Recognize text from an image (can be a local path, URL, or Buffer)
+    const ret = await worker.recognize('./assets/truefalsegrid.png');
+
+
+    // 3. Log the extracted text
+    console.log(ret.data.text);
+
+    // 4. Terminate the worker
+    await worker.terminate();
+
+})();
